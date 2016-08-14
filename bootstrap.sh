74061315d79c2ced14deeccaafb5260da296f8b8
Taishun Nakatani <teenak77@gmail.com>
2016-08-14 07:11:52 +0000

    Using rc.d scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,13 +9,18 @@ fi
 # There is 3 ways to work around this phenomenon.
 # 1. Use /sbin/init and let the service start from it.
 # 2. Run docker with --privileged option.
-# 3. Avoid using service command and just use the usual startup command 
+# 3. Avoid using service command and just use the daemon script directly
 # And my choice is 3. because the feature provided in service command is not actually needed at this morment.
 
 #service hadoop-hdfs-namenode start
-. /etc/default/hadoop
-. /etc/default/hadoop-hdfs-namenode
-su -s /bin/bash hdfs -c "cd /var/lib/hadoop-hdfs && /usr/lib/hadoop/sbin/hadoop-daemon.sh --config '$HADOOP_CONF_DIR' start namenode"
+/etc/rc3.d/S85hadoop-hdfs-namenode start
+
+if [ $? -gt 0 ]; then
+    echo "Starting daemon failed."
+    exit 1
+fi
+
+/etc/rc3.d/S85hadoop-yarn-resourcemanager start
 
 if [ $? -gt 0 ]; then
     echo "Starting daemon failed."
